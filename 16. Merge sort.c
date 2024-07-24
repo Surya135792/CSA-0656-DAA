2add731c1e9c4620083c10896ef1692bc5c05d44
Surya135792 <124139159+Surya135792@users.noreply.github.com>
2024-07-24 13:35:43 +0530

    16. Merge sort.c

---
+++ 16. Merge sort.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArray[n1], rightArray[n2];
+
+    for (i = 0; i < n1; i++)
+        leftArray[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        rightArray[j] = arr[mid + 1 + j];
+
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = left; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+    printf("A. Surya 192211797\n");
+
+    printf("Given array is \n");
+    printArray(arr, arrSize);
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arrSize);
+    return 0;
+}
